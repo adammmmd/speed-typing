9b88846c3e447ae6c8b1e281e5fd1afaa2dd702e
adammmmd <adam.muhammad.onduty@gmail.com>
2023-04-23 20:55:53 +0700

    add deploy setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 
 function App() {
+
   const [text, setText] = React.useState("")
   const [timeRemaining, setTimeRemaining] = React.useState(5)
-  const [isTimeRunning, setTImeRunning] = React.useState(false)
+  const [isTimeRunning, setTimeRunning] = React.useState(false)
   const [wordCount, setWordCount] = React.useState(0)
+  const inputRef = React.useRef(null)
+
   function handleChange(e) {
     const {value} = e.target
     setText(value)
@@ -16,24 +19,36 @@ function App() {
     return wordsArr.filter(word => word !== "").length
   }
 
+  function startGame() {
+    setTimeRemaining(5)
+    setTimeRunning(true)
+    setText("")
+    inputRef.current.disabled = false
+    inputRef.current.focus()
+  }
+
+  function endGame() {
+    setTimeRunning(false)
+      const numWords = calculateWordCount(text)
+      setWordCount(numWords)
+  }
+
   React.useEffect(() => {
     if (isTimeRunning && timeRemaining > 0) {
       setTimeout(() => {
         setTimeRemaining(time => time - 1)
       }, 1000)
     } else {
-      setTImeRunning(false)
-      const numWords = calculateWordCount(text)
-      setWordCount(numWords)
+      endGame()
     }
   }, [timeRemaining, isTimeRunning])
 
   return (
     <div>
       <h1>How fast do you type</h1>
-      <textarea onChange={handleChange} value={text}></textarea>
+      <textarea ref={inputRef} onChange={handleChange} value={text} disabled={!isTimeRunning}></textarea>
       <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={() => setTImeRunning(true)}>Start</button>
+      <button onClick={startGame()} disabled={isTimeRunning}>Start</button>
       <h1>Word Count: {wordCount} </h1>
     </div>
   )
