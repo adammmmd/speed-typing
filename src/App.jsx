73db5d09960d6e6548fa28780dbb24e255d993ec
adammmmd <adam.muhammad.onduty@gmail.com>
2023-04-25 18:35:19 +0700

    change ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import useWordGame from './hooks/useWordGame'
-
+import React, {useState, useEffect, useRef} from "react"
+import useWordGame from "./hooks/useWordGame"
 
 function App() {
-  const {inputRef, handleChange, text, isTimeRunning, timeRemaining, startGame, wordCount} = useWordGame()
-
-  return (
-    <div>
-      <h1>How fast do you type</h1>
-      <textarea ref={inputRef} onChange={handleChange} value={text} disabled={!isTimeRunning}></textarea>
-      <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={startGame()} disabled={isTimeRunning}>Start</button>
-      <h1>Word Count: {wordCount} </h1>
-    </div>
-  )
+    const {
+        textBoxRef, 
+        handleChange, 
+        text, 
+        isTimeRunning, 
+        timeRemaining, 
+        startGame, 
+        wordCount
+    } = useWordGame(5)
+    
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea
+                ref={textBoxRef}
+                onChange={handleChange}
+                value={text}
+                disabled={!isTimeRunning}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button 
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >
+                Start
+            </button>
+            <h1>Word count: {wordCount}</h1>
+        </div>
+    )
 }
 
 export default App
--- src/hooks/useWordGame.jsx
+++ src/hooks/useWordGame.jsx
@@ -1,48 +1,46 @@
-import React from "react"
+import {useState, useEffect, useRef} from "react"
 
 function useWordGame(startingTime = 10) {
-    const [text, setText] = React.useState("")
-    const [timeRemaining, setTimeRemaining] = React.useState(startingTime)
-    const [isTimeRunning, setTimeRunning] = React.useState(false)
-    const [wordCount, setWordCount] = React.useState(0)
-    const inputRef = React.useRef(null)
+    const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(startingTime)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
+    const [wordCount, setWordCount] = useState(0)
+    const textBoxRef = useRef(null)
     
     function handleChange(e) {
-      const {value} = e.target
-      setText(value)
+        const {value} = e.target
+        setText(value)
     }
     
     function calculateWordCount(text) {
-      const wordsArr = text.trim().split(" ")
-      return wordsArr.filter(word => word !== "").length
+        const wordsArr = text.trim().split(" ")
+        return wordsArr.filter(word => word !== "").length
     }
     
     function startGame() {
-      setTimeRemaining(startingTime)
-      setTimeRunning(true)
-      setText("")
-      inputRef.current.disabled = false
-      inputRef.current.focus()
+        setIsTimeRunning(true)
+        setTimeRemaining(startingTime)
+        setText("")
+        textBoxRef.current.disabled = false
+        textBoxRef.current.focus()
     }
     
     function endGame() {
-      setTimeRunning(false)
-        const numWords = calculateWordCount(text)
-        setWordCount(numWords)
+        setIsTimeRunning(false)
+        setWordCount(calculateWordCount(text))
     }
     
-    React.useEffect(() => {
-      if (isTimeRunning && timeRemaining > 0) {
-        setTimeout(() => {
-          setTimeRemaining(time => time - 1)
-        }, 1000)
-      } else {
-        endGame()
-      }
+    useEffect(() => {
+        if(isTimeRunning && timeRemaining > 0) {
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        } else if(timeRemaining === 0) {
+            endGame()
+        }
     }, [timeRemaining, isTimeRunning])
-
-    return {inputRef, handleChange, text, isTimeRunning, timeRemaining, startGame, wordCount}
+    
+    return {textBoxRef, handleChange, text, isTimeRunning, timeRemaining, startGame, wordCount}
 }
 
 export default useWordGame
-
